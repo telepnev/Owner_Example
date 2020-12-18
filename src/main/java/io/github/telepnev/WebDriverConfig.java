be93349424eb475470f725579404407346c45ba9
Evgeniy Telepnev <telepnef@yandex.ru>
2020-12-18 15:39:25 +0300

    added Owner lib

--- src/main/java/io/github/telepnev/WebDriverConfig.java
+++ src/main/java/io/github/telepnev/WebDriverConfig.java
@@ -2,8 +2,20 @@ package io.github.telepnev;
 
 import org.aeonbits.owner.Config;
 
-public interface WebDriverConfig extends Config {
+import java.net.URL;
 
+
+public interface WebDriverConfig extends Config {
+    @DefaultValue("FIREFOX")
+    @Key("webdriver.browser.name")
     BrowserName browserName();
+
+    @DefaultValue("http://localhost:4444/wd/hub")
+    @Key("webdriver.remote.url")
+    URL remoteURL();
+
+    @DefaultValue("false")
+    @Key("webdriver.remote")
+    boolean remote();
 
 }
--- src/main/java/io/github/telepnev/WebDriverProvider.java
+++ src/main/java/io/github/telepnev/WebDriverProvider.java
@@ -1,47 +1,33 @@
 package io.github.telepnev;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.function.Supplier;
 
 public class WebDriverProvider implements Supplier<WebDriver> {
     @Override
     public WebDriver get() {
-        final String isRemoteString = System.getProperty("webdriver.remote");
-        final boolean isRemote = Boolean.parseBoolean(isRemoteString);
-        if (isRemote) {
-            final String remoteUrlString = System.getProperty("webdriver.remote.url");
-            try {
-                final URL remoteURL = new URL(remoteUrlString);
-                return new RemoteWebDriver(remoteURL, DesiredCapabilities.chrome());
+        final WebDriverConfig config = ConfigFactory.newInstance().create(WebDriverConfig.class);
 
-            } catch (MalformedURLException e) {
-               throw new RuntimeException(e);
-
-            }
+        if (config.remote()) {
+          return new RemoteWebDriver(config.remoteURL(),DesiredCapabilities.chrome());
         } else {
-            final String browserName = System.getProperty("browser.name");
-            if ("chrome".equals(browserName)) {
-                WebDriverManager.chromedriver().setup();
-                return new ChromeDriver();
+            switch (config.browserName()) {
+                case CHROME:
+                    WebDriverManager.chromedriver().setup();
+                    return new ChromeDriver();
+                case FIREFOX:
+                    WebDriverManager.firefoxdriver().setup();
+                    return new FirefoxDriver();
+                default:
+                    throw new RuntimeException("Unknown browser name: " + config.browserName());
             }
-            if ("firefox".equals(browserName)) {
-                WebDriverManager.firefoxdriver().setup();
-                return new FirefoxDriver();
-            }
-            throw new RuntimeException("Unknown browser name: " + browserName);
         }
     }
-
-    private String getBrowserName() {
-        return System.getProperty("browser.name", "chrome");
-    }
-
 }
